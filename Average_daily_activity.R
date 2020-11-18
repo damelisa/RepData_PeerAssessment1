ea6e743e9a190e9f9ef90cf05705ad5d695d8366
damelisa <kalidami@gmail.com>
2020-11-18 21:43:17 +0100

    averaged daily activity of mean total steps over intervals

---
+++ Average_daily_activity.R
@@ -0,0 +1,12 @@
+library(dplyr)
+library(ggplot2)
+
+data_activity <- data %>% 
+  group_by(date) %>%
+  mutate(interval = 1:n()) %>%
+  group_by(interval) %>%
+  summarise(steps = mean(steps, na.rm = TRUE))
+
+ggplot(data_activity, aes(x = interval, y = steps)) + geom_line()
+
+max_interval <- c(data_activity[[which.max(data_activity$steps),1]])
